fd0c5c2e68c1bb1cbd4173f528bcf5071889c4e5
Yulya <juliya.insider@gmail.com>
2019-12-06 18:04:46 +0300

    feat: Create deactivation module

--- portal/src/main/scala/cromwell/pipeline/datastorage/DatastorageModule.scala
+++ portal/src/main/scala/cromwell/pipeline/datastorage/DatastorageModule.scala
@@ -6,10 +6,8 @@ import cromwell.pipeline.database.PipelineDatabaseEngine
 import cromwell.pipeline.datastorage.dao.entry.UserEntry
 import cromwell.pipeline.datastorage.dao.repository.UserRepository
 
-import scala.concurrent.ExecutionContext
-
 @Module
-class DatastorageModule(config: Config)  (implicit executionContext: ExecutionContext){
+class DatastorageModule(config: Config) {
   lazy val pipelineDatabaseEngine: PipelineDatabaseEngine = wireWith(PipelineDatabaseEngine.fromConfig _)
   lazy val userEntry: UserEntry = wire[UserEntry]
   lazy val userRepository: UserRepository = wire[UserRepository]
